4062b2e8e4a8483ccc9dd1a3466df3b832a25536
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-02-25 16:50:13 +0530

    Connected to the firebase.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC4otNNBDy9c7gtG9XWzvoKGkw1G8BD2q0",
+    authDomain: "slack-clone-challange-c86e3.firebaseapp.com",
+    projectId: "slack-clone-challange-c86e3",
+    storageBucket: "slack-clone-challange-c86e3.appspot.com",
+    messagingSenderId: "82777229766",
+    appId: "1:82777229766:web:23a5c829f55d887ceb4ac7",
+    measurementId: "G-0Q284Y3M3F"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+
+
+  export default db;
